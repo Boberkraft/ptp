c35ba7b1ec151e4fd4206e7fd4ff602fdc9dc8ec
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2020-06-15 19:04:50 +0200

    Adi (#7)
    Co-authored-by: Adi52 <adimr52@gmail.com>

--- program/src/prediction.cpp
+++ program/src/prediction.cpp
@@ -1,22 +1,6 @@
 #include "prediction.hpp"
-#include "series.hpp"
-#include "arithmetic.hpp"
-#include "geometric.hpp"
 
 void print(unique_ptr<Prediction> prediction) {
     cout << "It's " << prediction->series_name
          << ". Next number is " << prediction->next_number << endl;
-}
-
-unique_ptr<Prediction> predict(vector<int> given) {
-    vector<Series *> series_types;
-    series_types.emplace_back(new Arithmetic);
-    series_types.emplace_back(new Geometric);
-
-    for (auto &type : series_types) {
-        if (type->is_a(given)) {
-            return unique_ptr<Prediction>(type->get_prediction(given));
-        }
-    }
-    return nullptr;
 }
